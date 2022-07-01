7b510bb6dafcc0440583f8ca01d6c8443ff9d183
perchik_air <1919ars@gmail.com>
2022-07-02 00:08:12 +0300

    more

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## Credits
 
 - [GitHub | Learning MongoDB](https://github.com/Arseni1919/Learning_mongodb)
-- []()
+- [PyTorch | NN](https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html)
 - []()
 - []()
 - 
--- impl_simple_nn.py
+++ impl_simple_nn.py
@@ -1,8 +1,27 @@
 from GLOBALS import *
+from impl_get_data import get_data_from_pickle
+
+
+def run_simple_nn(x, y):
+    # load a model
+    pass
+
+    # train the model
+    pass
+
+    # save the model
+    pass
+
+    # test the model
+    pass
+
+    # plot results
+    pass
 
 
 def main():
-    pass
+    x, y = get_data_from_pickle()
+    run_simple_nn(x, y)
 
 
 if __name__ == '__main__':
